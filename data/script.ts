ca8c3970f362494ea1d173781b9741516575dc17
Stenio Elson <contato@stenioelson.com.br>
2014-04-10 23:22:45 -0300

    Update script.ts

--- data/script.ts
+++ data/script.ts
@@ -69,7 +69,7 @@ class Particula {
 		
 		retorno = this.calcVelocidade(2, x, y);
 		
-		$("#eter").html(vEter);
+		$("#vEter").html(vEter);
 		$("#velocidade").html(retorno);
 		$("#grau").html(inclinacao);
 		$("#comprimento").html(this.plano.getComprimento());
